3009ca318b74843bb1f488f55bc76c854635130d
Samuel Swagger <fm0776hp@go.minnstate.edu>
2026-03-03 22:34:25 -0600

    reduced redundant header and footer made minor changes

---
+++ src/js/basic.js
@@ -0,0 +1,42 @@
+// =============
+// Basic Scripts
+// =============
+
+// find and return text files
+async function getResources(path) {
+	return fetch(path).then((res) => res.text()).then((text) => {
+		return text;
+	})
+}
+
+// ==============
+// Header Scripts
+// ==============
+
+// when the page is loaded, get the header and add nav scripts
+window.addEventListener("load", async function () {
+	// get the header
+	document.getElementById("header").innerHTML = await getResources("res/header.html");
+
+	// navigation scripts for small screens
+	let menuOpen = false;
+	document.getElementById("navLink").addEventListener("click", function () {
+		if (!menuOpen) {
+			document.getElementById("quickLinks").style.display = "block";
+		} else {
+			document.getElementById("quickLinks").style.display = "none";
+		}
+		menuOpen = !menuOpen
+		return false;
+	});
+});
+
+// ==============
+// Footer Scripts
+// ==============
+
+// when the page is loaded, retrieve the footer
+window.addEventListener("load", async function () {
+	// get the header
+	document.getElementById("footer").innerHTML = await getResources("res/footer.html");
+});
